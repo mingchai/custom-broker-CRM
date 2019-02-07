958c733f33608d7fd4691988416d9786a4cd57b1
Ming Chai <ming.chai4@gmail.com>
2019-02-06 21:11:41 -0800

    create full_name method and email validation for clients model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,10 @@
 class Client < ApplicationRecord
+    validates :email,
+    presence: true, 
+    uniqueness: true, 
+    format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+    def full_name
+        "#{first_name} #{last_name}".strip
+    end
 end
