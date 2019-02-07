ca03ee4aa8bd02978a12b3e8fb34eca84433942f
Ming Chai <ming.chai4@gmail.com>
2019-02-06 20:13:17 -0800

    user create and udpate functioning. Modified user model validations and methods. Edited form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email,
+    presence: true, 
+    uniqueness: true, 
+    format: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+    def full_name
+        "#{first_name} #{last_name}".strip
+    end
 end
