f461531ae7c0447f543a9e8fe29cb253d2ebba17
Ming Chai <ming.chai4@gmail.com>
2019-02-06 21:40:47 -0800

    modify migration file and migrated forward

--- db/migrate/20190207052920_create_policies.rb
+++ db/migrate/20190207052920_create_policies.rb
@@ -1,7 +1,9 @@
 class CreatePolicies < ActiveRecord::Migration[5.2]
   def change
     create_table :policies do |t|
-
+      t.string "policy_number", index: {unique: true}
+      t.float "annual_premium"
+      t.references :client, foreign_key: true
       t.timestamps
     end
   end
