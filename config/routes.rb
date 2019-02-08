505a8a74e28e0440c58a6e3325cf8fbb64aa2445
Ming Chai <ming.chai4@gmail.com>
2019-02-07 18:52:49 -0800

    add client API route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :session, only: [:create, :destroy]
+      resources :clients
       resources :users, only:[] do
         get :current, on: :collection
       end
