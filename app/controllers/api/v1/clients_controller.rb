b1b0166fa63d7274edf92ec77e74abe68490db55
Ming Chai <ming.chai4@gmail.com>
2019-02-07 19:13:43 -0800

    add client#create action in API controller

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ClientsController < ApplicationController
+class Api::V1::ClientsController < Api::ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     def index
         clients = Client.all
@@ -9,8 +9,40 @@ class Api::V1::ClientsController < ApplicationController
         render json: client
     end
 
+    def create
+        client = Client.new(client_params)
+        client.broker_id = current_user.id
+
+      if client.save!
+         render json:{first_name: client.first_name,
+         last_name: client.last_name,
+         phone_number: client.phone_number,
+         email: client.email,
+         street_address: client.street_address,
+         city: client.city,
+         province: client.province,
+         postal_code: client.postal_code,
+         marketing_consent: client.marketing_consent,
+         broker_id: client.broker_id }
+      else
+        render json: {errors: @client.errors, status: :unprocessable_entity }
+      end
+    end
+
     private
     def client
         @client ||= Client.find params[:id]
     end
+
+    def client_params
+        params.require(:client).permit(:first_name,
+        :last_name,
+        :phone_number,
+        :email,
+        :street_address,
+        :city,
+        :province,
+        :postal_code,
+        :marketing_consent)
+      end
 end
