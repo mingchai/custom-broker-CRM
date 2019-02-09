332c9d7695ca6844257a0533b4d1f768156ee49f
Ming Chai <ming.chai4@gmail.com>
2019-02-08 18:45:43 -0800

    modify create, find user , and user params methods

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController < Api::ApplicationController
   def index
     users = User.all
     render json: users
@@ -8,8 +8,20 @@ class Api::V1::UsersController < ApplicationController
       render json: user
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      render json: user
+    else
+      render json: {errors: user.errors, status:422}
+    end
+  end
+
 private
   def user
-    @user ||= Client.find params[:id]
+    @user ||= User.find params[:id]
+  end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
   end
 end
