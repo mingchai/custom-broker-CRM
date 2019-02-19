f27cc6f24e752995ccea1a2b05dd33fd7eb005ac
Ming Chai <ming.chai4@gmail.com>
2019-02-18 19:38:15 -0800

    add styling changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         if user&.authenticate(params[:password])
             session[:user_id] = user.id
             flash[:primary] = "Welcome, #{user.first_name}!"
-            redirect_to root_path
+            redirect_to clients_path
         else
             flash[:danger] = "Email or password is incorrect"
             render :new
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
     def destroy
         session[:user_id] = nil
         flash[:primary] = "Logged Out"
-        redirect_to root_path
+        redirect_to new_session_path
     end
 end
