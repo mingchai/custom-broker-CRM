ec671c0a9d0b796603b6c7b99fe73ba266441aac
Ming Chai <ming.chai4@gmail.com>
2019-04-09 14:54:20 -0700

    configure rack-cors

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,13 @@ module CustomCrm
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000', '127.0.0.1:3000', 'localhost:3001'
+        resource '/api/*', headers: :any, credentials: true, methods: [:get, :post, :delete, :patch, :put, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
