923308d0b1f994ce87196236f63d51b858a363f7
Ming Chai <ming.chai4@gmail.com>
2019-04-09 14:51:17 -0700

    add additional API routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,24 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
+      
       resource :session, only: [:create, :destroy]
-      resources :clients
+
+      resources :clients do
+        post("/call", to: "clients#call")
+      end
+
       resources :users, only:[] do
         get :current, on: :collection
       end
+
       resources :users
+
+      resources :policies, only: [] do
+        resources :payments, only: [:new, :create]
+       end
+    
+      resources :tokens, only: [:create]
     end
   end
 end
