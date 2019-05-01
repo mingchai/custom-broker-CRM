9e0b7e61b5c9ebf18cc57e8a2b52870c1373eecc
Ming Chai <ming.chai4@gmail.com>
2019-04-30 21:27:17 -0700

    add more origins for CORS

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module CustomCrm
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', '127.0.0.1:3000', 'localhost:3001'
+        origins 'localhost:3000', '127.0.0.1:3000', 'localhost:3001', '127.0.0.1:3001', 'localhost:3002', '127.0.0.1:3002'
         resource '/api/*', headers: :any, credentials: true, methods: [:get, :post, :delete, :patch, :put, :options]
       end
     end
